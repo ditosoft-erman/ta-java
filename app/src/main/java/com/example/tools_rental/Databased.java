91618e1d23a19de5615bf70330e06b38a3d23816
unknown <ermanfaminiano020@gmail.com>
2024-11-08 17:24:42 +0800

    new app

---
+++ app/src/main/java/com/example/tools_rental/Databased.java
@@ -0,0 +1,5 @@
+package com.example.tools_rental;
+
+public class Databased {
+
+}
---
+++ app/src/main/java/com/example/tools_rental/View/user_profile.java
@@ -0,0 +1,30 @@
+package com.example.tools_rental.View;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tools_rental.R;
+
+public class user_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_user_profile);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
+
+
+
+
